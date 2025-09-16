aaf0b33374ff0813c60cdb0da76ef6172ce69964
somchaure5555 <somchaure5555@gmail.com>
2025-09-16 11:08:53 -0700

    Add files via upload
    simple mathematic program and string program

---
+++ Assignment_1.py
@@ -0,0 +1,23 @@
+
+#task 1
+a= input('Enter the First Number :-')
+a1=int(a)
+b= input('Enter the Second Number :-')
+b1=int(b)
+sum=a1+b1
+print("Addition :- ",sum)
+sub=a1-b1
+print("subtraction :- ", sub)
+multi=a1*b1
+print("Multiplication :- ", multi)
+div= a1-b1
+d=float(div)
+print("division :- ", d)
+
+#task 2
+
+a2= input("Enter the First Name :- ")
+b2= input("Enter the Last Name :- ")
+full_name=a2+ b2
+
+print("Hello, "+ full_name +"! Welcome to the Python Program")
